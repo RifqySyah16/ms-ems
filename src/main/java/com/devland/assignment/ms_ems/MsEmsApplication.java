97658ae97da448142b8cc3b2a47f65fa8f0295e9
Muhammad Rifqy Syah <rifqy.syah.harahap@gmail.com>
2024-10-16 21:22:35 +0700

    configuration repo

---
+++ src/main/java/com/devland/assignment/ms_ems/MsEmsApplication.java
@@ -0,0 +1,13 @@
+package com.devland.assignment.ms_ems;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsEmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsEmsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devland/assignment/ms_ems/MsEmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devland.assignment.ms_ems;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsEmsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
